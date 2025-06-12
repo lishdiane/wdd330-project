451051f0f88edc41d578a7d57b3082423ec65df5
lishdiane <dlish@byupathway.edu>
2025-06-11 18:48:40 -0600

    d

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -1,23 +1,23 @@
 //-------Load Templates---------//
 
-export async function loadTemplate(path) {
-    const res = await fetch(path);
-    const template = await res.text();
-    return template;
-}
+// export async function loadTemplate(path) {
+//     const res = await fetch(path);
+//     const template = await res.text();
+//     return template;
+// }
 
-//header and footer
-export async function loadHeaderFooter() {
-    const headerTemplate = await loadTemplate("./partials/header.html");
-    const headerElement = document.querySelector(".main-header");
-    headerElement.innerHTML = headerTemplate;
-    showMenu()
+// //header and footer
+// export async function loadHeaderFooter() {
+//     const headerTemplate = await loadTemplate("./partials/header.html");
+//     const headerElement = document.querySelector(".main-header");
+//     headerElement.innerHTML = headerTemplate;
+//     showMenu()
 
-    const footerTemplate = await loadTemplate("./partials/footer.html");
-    const footerElement = document.querySelector(".main-footer");
-    footerElement.innerHTML = footerTemplate;
-    buildFooterContent()
-}
+//     const footerTemplate = await loadTemplate("./partials/footer.html");
+//     const footerElement = document.querySelector(".main-footer");
+//     footerElement.innerHTML = footerTemplate;
+//     buildFooterContent()
+// }
 
 function buildFooterContent() {
     //get year and modification date and insert into footer
